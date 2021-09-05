2fc5362d06ec7e082c468696cc2ef2d80820fa26
LuiCoro <luiacorona029@icloud.com>
2021-09-04 20:41:48 -0700

    'Installed Axios' - Created a variable that stores our data

--- github/src/App.js
+++ github/src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
-  
   render() {
+  
+    const axiosPromise =
+      axios.get('https://api.github.com/users/LuiCoro')
+        .then(res => {
+          const MyData = res.data
+          console.log(MyData)
+        })
+        .catch(err => {
+          console.log('It Broke!' , err)
+        })
+    
+    console.log(axiosPromise)
+    
     return (
       <div>
         <h1>Hello World</h1>
